c83e3a4ed9e625d5a4a8d21630161523c2382171
SHAHRIAR IBNE ALAM MAHEE <maheecse2030@gmail.com>
2026-08-20 21:11:35 +0600

    added 900 rating B

---
+++ 900/B_ezraft_and_Array.cpp
@@ -0,0 +1,77 @@
+/// BISMILLAHIR RAHMANIR RAHEEM
+/// @cf - maheecse
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ld long double
+#define vi vector<int>
+#define vll vector<long long>
+#define pii pair<int, int>
+#define pll pair<long long, long long>
+#define pb push_back
+#define fi first
+#define se second
+
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) ((int)(x).size())
+
+#define FOR(i, a, b) for (int i = (a); i < (b); i++)
+#define FORR(i, a, b) for (int i = (a); i >= (b); i--)
+
+using i128 = __int128_t;
+using u128 = __uint128_t;
+
+const int MOD = 1e9 + 7;
+const ll INF = 1e18;
+
+void cp_chere_dibo()
+{
+    int n;
+    cin >> n;
+    if (n == 1)
+    {
+        cout << 1 << endl;
+        return;
+    }
+    else if (n == 2)
+    {
+        cout << -1 << endl;
+        return;
+    }
+    vll a(n);
+
+    a[0] = 1;
+    a[1] = 2;
+    a[2] = 3;
+
+    FOR(i, 3, n)
+    {
+        a[i] = a[i - 1] * 2;
+    }
+
+    FOR(i, 0, n)
+    {
+        cout << a[i] << " ";
+    }
+
+    cout << endl;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int tc = 1;
+    cin >> tc;
+
+    while (tc--)
+    {
+        cp_chere_dibo();
+    }
+
+    return 0;
+}
