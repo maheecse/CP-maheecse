b589bb23d28b5b5e144d9aab6766e0f3fd5b53a0
SHAHRIAR IBNE ALAM MAHEE <maheecse2030@gmail.com>
2026-08-26 00:20:30 +0600

    1100 rated

---
+++ 1100/C_Long_Jumps.cpp
@@ -0,0 +1,79 @@
+/// BISMILLAHIR RAHMANIR RAHEEM
+/// @cf - maheecse
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ld long double
+#define vi vector<int>
+#define vll vector<long long>
+#define pii pair<int, int>
+#define pll pair<long long, long long>
+#define pb push_back
+#define fi first
+#define se second
+
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) ((int)(x).size())
+
+#define FOR(i, a, b) for (int i = (a); i < (b); i++)
+#define FORR(i, a, b) for (int i = (a); i >= (b); i--)
+
+using i128 = __int128_t;
+using u128 = __uint128_t;
+
+const int MOD = 1e9 + 7;
+const ll INF = 1e18;
+
+void cp_chere_dibo()
+{
+    int n;
+    cin >> n;
+
+    int a[n];
+    ll s[n];
+
+    FOR(i, 0, n)
+    {
+        cin >> a[i];
+    }
+
+    ll ans = 0;
+
+    for (int i = n - 1; i >= 0; i--)
+    {
+
+        int x = i + a[i];
+
+        if (x >= n)
+        {
+            s[i] = a[i];
+        }
+        else
+        {
+            s[i] = a[i] + s[x];
+        }
+
+        ans = max(ans, s[i]);
+    }
+
+    cout << ans << endl;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int tc = 1;
+    cin >> tc;
+
+    while (tc--)
+    {
+        cp_chere_dibo();
+    }
+
+    return 0;
+}
