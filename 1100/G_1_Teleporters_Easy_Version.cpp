0ad75bf3efeea9ecf13d8f71f24cedeac97c4d91
SHAHRIAR IBNE ALAM MAHEE <maheecse2030@gmail.com>
2026-08-27 20:41:04 +0600

    1100 G1 easy version

---
+++ 1100/G_1_Teleporters_Easy_Version.cpp
@@ -0,0 +1,75 @@
+/// BISMILLAHIR RAHMANIR RAHEEM
+/// @cf - maheecse
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ld long double
+#define vi vector<int>
+#define vll vector<long long>
+#define pii pair<int, int>
+#define pll pair<long long, long long>
+#define pb push_back
+#define fi first
+#define se second
+
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) ((int)(x).size())
+
+#define FOR(i, a, b) for (int i = (a); i < (b); i++)
+#define FORR(i, a, b) for (int i = (a); i >= (b); i--)
+
+using i128 = __int128_t;
+using u128 = __uint128_t;
+
+const int MOD = 1e9 + 7;
+const ll INF = 1e18;
+
+void cp_chere_dibo()
+{
+    ll n, c;
+    cin >> n >> c;
+    vll v(n);
+    FOR(i, 0, n)
+    {
+        cin >> v[i];
+        v[i] += i + 1;
+    }
+    sort(all(v));
+
+    ll sum = 0;
+    ll ans = 0;
+
+    FOR(i, 0, n)
+    {
+        if (sum + v[i] <= c)
+        {
+            sum += v[i];
+            ans++;
+        }
+        else
+        {
+            break;
+        }
+    }
+
+    cout << ans << endl;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int tc = 1;
+    cin >> tc;
+
+    while (tc--)
+    {
+        cp_chere_dibo();
+    }
+
+    return 0;
+}
