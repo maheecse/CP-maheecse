2af671fafe9d001fac214233346859ad8e6ed97b
Shahriar Ibne Alam Mahee <maheecse2030@gmail.com>
2026-09-02 19:04:50 +0600

    1300 rated

---
+++ B_problems_CF/B_T_primes.cpp
@@ -0,0 +1,108 @@
+/// BISMILLAHIR RAHMANIR RAHEEM
+
+/// @cf - maheecse
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ld long double
+#define vi vector<int>
+#define vll vector<long long>
+#define pii pair<int, int>
+#define pll pair<long long, long long>
+#define pb push_back
+#define fi first
+#define se second
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) ((int)(x).size())
+#define FOR(i, a, b) for (int i = (a); i < (b); i++)
+#define FORR(i, a, b) for (int i = (a); i >= (b); i--)
+
+using i128 = __int128_t;
+using u128 = __uint128_t;
+
+const int MOD = 1e9 + 7;
+const ll INF = 1e18;
+
+// int countDivisors(ll n)
+// {
+//     int count = 0;
+
+//     for (ll i = 1; i * i <= n; i++)
+//     {
+//         if (n % i == 0)
+//         {
+//             count++;
+
+//             if (i != n / i)
+//                 count++;
+//         }
+//     }
+
+//     return count;
+// }
+
+// void cp_chere_dibo()
+// {
+//     ll n;
+//     cin >> n;
+
+//     vll v(n);
+
+//     FOR(i, 0, n)
+//     {
+//         cin >> v[i];
+
+//         if (countDivisors(v[i]) == 3)
+//         {
+//             cout << "YES" << endl;
+//         }
+//         else
+//         {
+//             cout << "NO" << endl;
+//         }
+//     }
+// }
+bool isPrime(ll n)
+{
+    if (n < 2)
+        return false;
+
+    for (ll i = 2; i * i <= n; i++)
+    {
+        if (n % i == 0)
+            return false;
+    }
+
+    return true;
+}
+void cp_chere_dibo()
+{
+    ll x;
+    cin >> x;
+
+    ll y = sqrt(x);
+
+    if (y * y == x && isPrime(y))
+        cout << "YES\n";
+    else
+        cout << "NO\n";
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int tc = 1;
+    cin >> tc;
+
+    while (tc--)
+    {
+        cp_chere_dibo();
+    }
+
+    return 0;
+}
