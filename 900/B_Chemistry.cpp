83503e28fa027f9ab742569077cedad1778d317d
Shahriar Ibne Alam Mahee <maheecse2030@gmail.com>
2026-09-04 22:54:28 +0600

    900 rated B problem
    Add a C++ solution for Codeforces problem B (Chemistry). The implementation reads multiple test cases, counts character frequencies, computes the number of odd-frequency letters and prints YES if odd <= k+1 otherwise NO. Files added at 900/B_Chemistry.cpp and B_problems_CF/B_Chemistry.cpp.

---
+++ 900/B_Chemistry.cpp
@@ -0,0 +1,79 @@
+/// BISMILLAHIR RAHMANIR RAHEEM
+/// @cf - maheecse
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ld long double
+#define vi vector<int>
+#define vll vector<long long>
+#define pii pair<int, int>
+#define pll pair<long long, long long>
+#define pb push_back
+#define fi first
+#define se second
+
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) ((int)(x).size())
+
+#define FOR(i, a, b) for (int i = (a); i < (b); i++)
+#define FORR(i, a, b) for (int i = (a); i >= (b); i--)
+
+using i128 = __int128_t;
+using u128 = __uint128_t;
+
+const int MOD = 1e9 + 7;
+const ll INF = 1e18;
+
+void cp_chere_dibo()
+{
+    int n, k;
+    cin >> n >> k;
+
+    string s;
+    cin >> s;
+
+    int cnt[26] = {0};
+
+    for (int i = 0; i < n; i++)
+    {
+        cnt[s[i] - 'a']++;
+    }
+
+    int odd = 0;
+
+    for (int i = 0; i < 26; i++)
+    {
+        if (cnt[i] % 2 != 0)
+        {
+            odd++;
+        }
+    }
+
+    if (odd <= k + 1)
+    {
+        cout << "YES" << endl;
+    }
+    else
+    {
+        cout << "NO" << endl;
+    }
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int tc = 1;
+    cin >> tc;
+
+    while (tc--)
+    {
+        cp_chere_dibo();
+    }
+
+    return 0;
+}
