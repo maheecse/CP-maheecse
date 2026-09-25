10b1a6fe03b0b70ff51bbedf6469480e4e99a711
Shahriar Ibne Alam Mahee <maheecse2030@gmail.com>
2026-09-25 23:11:24 +0600

    div 2

---
+++ ALL CF CONTEST/A_Turn_Into_a_Palindrome.cpp
@@ -0,0 +1,65 @@
+/// BISMILLAHIR RAHMANIR RAHEEM
+/// @cf - maheecse
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ld long double
+#define vi vector<int>
+#define vll vector<long long>
+#define pii pair<int, int>
+#define pll pair<long long, long long>
+#define pb push_back
+#define fi first
+#define se second
+
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) ((int)(x).size())
+
+#define FOR(i, a, b) for (int i = (a); i < (b); i++)
+#define FORR(i, a, b) for (int i = (a); i >= (b); i--)
+
+using i128 = __int128_t;
+using u128 = __uint128_t;
+
+const int MOD = 1e9 + 7;
+const ll INF = 1e18;
+
+void cp_chere_dibo()
+{
+    int n;
+    string s;
+    char c;
+    cin >> n >> c;
+    cin >> s;
+    int cnt = 0;
+    for (int i = 0; i < n / 2; i++)
+    {
+        if (s[i] != s[n - i - 1])
+        {
+            if (s[i] == c || s[n - i - 1] == c)
+                cnt += 1;
+            else
+                cnt += 2;
+        }
+    }
+    cout << cnt << endl;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int tc = 1;
+    cin >> tc;
+
+    while (tc--)
+    {
+        cp_chere_dibo();
+    }
+
+    return 0;
+}
